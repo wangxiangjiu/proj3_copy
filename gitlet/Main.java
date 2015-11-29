7065be41cd668013424862dbb34d127e1a943e66
Salvador Villegas <villegas@berkeley.edu>
2015-11-29 10:03:03 -0800

    everything

--- gitlet/Main.java
+++ gitlet/Main.java
@@ -1,14 +1,31 @@
 package gitlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Commands.*;
+
 /** Driver class for Gitlet, the tiny stupid version-control system.
  *  @author Xiangjiu Wang.
  */
 public class Main {
-
+    
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND> .... */
     public static void main(String... args) {
-        // FILL THIS IN
+        
+        if (args.length == 0) {
+            System.err.println("Please put arguments");
+            return;
+        }
+        
+        CommandInterpreter cp = new CommandInterpreter(args);
+        
+        
+        
+        
     }
+
+    
 
 }
--- gitlet/UnitTest.java
+++ gitlet/UnitTest.java
@@ -9,19 +9,33 @@ import static org.junit.Assert.*;
  */
 public class UnitTest {
 
-    /** Run the JUnit tests in the loa package. Add xxxTest.class entries to
+    /** Run the JUnit tests in the gitLet package. Add xxxTest.class entries to
      *  the arguments of runClasses to run other JUnit tests. */
     public static void main(String[] ignored) {
         textui.runClasses(UnitTest.class);
     }
 
-    @Test
-    public void dummy() {}
     /** A dummy test to avoid complaint. */
     @Test
     public void placeholderTest() {
     }
-
+    
+    @Test
+    public void testCommit() {
+        Commit p1 = new Commit();
+        Commit s1 = new Commit(0L, "", null, p1);
+        Commit s2 = new Commit(0L, "", null, p1);
+        assertEquals(false, p1._id == s1._id);
+        assertEquals(false, s1._id == s2._id);
+        
+    }
+    
+    @Test
+    public void testInit() {
+        String[] string = new String[1];
+        string[0] = "init";
+        CommandInterpreter inter = new CommandInterpreter(string);
+    }
 }
 
 
